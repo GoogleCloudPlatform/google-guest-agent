5c8776fb1db0f105377f0239e5bbe1bde18e75fb
Chaitanya Kulkarni <chaitanyauk@google.com>
2025-03-07 15:00:40 -0800

    Implement compat layer for metadata script runner. Launch new script runner or legacy based on core plugin enable settings.
    PiperOrigin-RevId: 734699555

---
+++ cmd/metadata_script_runner_compat/metadata_script_runner_compat.go
@@ -0,0 +1,117 @@
+//  Copyright 2025 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+// Package main is the entry point for the metadata script runner compat binary.
+// It is a wrapper that either runs the metadata script runner or the legacy
+// metadata script runner based on core plugin configuration.
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/GoogleCloudPlatform/galog"
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/cfg"
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/logger"
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/metadata"
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/run"
+)
+
+var (
+	// version is the version of the binary.
+	version = "unknown"
+)
+
+const (
+	// galogShutdownTimeout is the period of time we should wait galog to
+	// shutdown.
+	galogShutdownTimeout = time.Second
+)
+
+func main() {
+	ctx := context.Background()
+
+	if err := cfg.Load(nil); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to load instance config:", err)
+		os.Exit(1)
+	}
+
+	coreCfg := cfg.Retrieve().Core
+	logOpts := logger.Options{
+		Ident:          "google_compat_metadata_script_runner",
+		CloudIdent:     "GCEGuestCompatMetadataScriptRunner",
+		ProgramVersion: version,
+		LogFile:        coreCfg.LogFile,
+		Level:          coreCfg.LogLevel,
+		Verbosity:      coreCfg.LogVerbosity,
+	}
+
+	if err := logger.Init(ctx, logOpts); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to initialize logger:", err)
+		os.Exit(1)
+	}
+	defer galog.Shutdown(galogShutdownTimeout)
+
+	if len(os.Args) != 2 {
+		galog.Fatalf("No valid event type (%v) provided, usage: %s <startup|shutdown|specialize>", os.Args, os.Args[0])
+	}
+
+	if err := launchScriptRunner(ctx, metadata.New(), os.Args[1]); err != nil {
+		galog.Fatalf("Failed to launch script runner: %v", err)
+	}
+	galog.Infof("Successfully launched script runner")
+}
+
+func launchScriptRunner(ctx context.Context, mdsClient metadata.MDSClientInterface, event string) error {
+	var enabled bool
+	opts := run.Options{
+		Name:       metadataScriptRunnerLegacy,
+		OutputType: run.OutputStream,
+		Args:       []string{event},
+	}
+
+	mds, err := mdsClient.Get(ctx)
+	if err != nil {
+		galog.Warnf("Failed to fetch MDS descriptor: [%v], falling back to legacy script runner", err)
+	} else {
+		if enabled = mds.HasCorePluginEnabled(); enabled {
+			opts.Name = metadataScriptRunnerNew
+		}
+	}
+
+	galog.Infof("Enable core plugin set to: [%t], launching script runner for event %q from %q", enabled, event, opts.Name)
+	res, err := run.WithContext(ctx, opts)
+	if err != nil {
+		return fmt.Errorf("failed to run script runner: %v", err)
+	}
+
+	streams := res.OutputScanners
+	// Go routines will exit once all output is consumed. Run library guarantees
+	// that all channels are closed after use.
+	go func() {
+		for line := range streams.StdOut {
+			galog.Info(line)
+		}
+	}()
+
+	go func() {
+		for line := range streams.StdErr {
+			galog.Error(line)
+		}
+	}()
+
+	return <-streams.Result
+}
---
+++ cmd/metadata_script_runner_compat/metadata_script_runner_compat_test.go
@@ -0,0 +1,179 @@
+//  Copyright 2025 Google LLC
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/metadata"
+	"github.com/GoogleCloudPlatform/google-guest-agent/internal/run"
+	"github.com/google/go-cmp/cmp"
+)
+
+// MDSClient implements fake metadata server.
+type MDSClient struct {
+	instanceEnable bool
+	throwErr       bool
+}
+
+const instanceMdsTemplate = `
+{
+  "instance": {
+    "attributes": {
+      "enable-guest-agent-core-plugin": "%t"
+    }
+  }
+}
+`
+
+// GetKeyRecursive implements fake GetKeyRecursive MDS method.
+func (s *MDSClient) GetKeyRecursive(ctx context.Context, key string) (string, error) {
+	return "", fmt.Errorf("not yet implemented")
+}
+
+// GetKey implements fake GetKey MDS method.
+func (s *MDSClient) GetKey(ctx context.Context, key string, headers map[string]string) (string, error) {
+	return "", fmt.Errorf("not yet implemented")
+}
+
+// Get method implements fake Get on MDS.
+func (s *MDSClient) Get(context.Context) (*metadata.Descriptor, error) {
+	if s.throwErr {
+		return nil, fmt.Errorf("test error")
+	}
+	jsonData := fmt.Sprintf(instanceMdsTemplate, s.instanceEnable)
+	return metadata.UnmarshalDescriptor(jsonData)
+}
+
+// Watch method implements fake watcher on MDS.
+func (s *MDSClient) Watch(context.Context) (*metadata.Descriptor, error) {
+	return nil, fmt.Errorf("not yet implemented")
+}
+
+// WriteGuestAttributes method implements fake writer on MDS.
+func (s *MDSClient) WriteGuestAttributes(context.Context, string, string) error {
+	return fmt.Errorf("not yet implemented")
+}
+
+type testRunner struct {
+	returnErr      bool
+	seenCommand    string
+	seenOutputType run.OutputType
+	seenArgs       []string
+}
+
+func (t *testRunner) WithContext(ctx context.Context, opts run.Options) (*run.Result, error) {
+	t.seenCommand = opts.Name
+	t.seenArgs = opts.Args
+	t.seenOutputType = opts.OutputType
+
+	stdoutstream := make(chan string)
+	stderrstream := make(chan string)
+	resultstream := make(chan error, 1)
+
+	if t.returnErr {
+		resultstream <- errors.New("error")
+	}
+
+	close(stdoutstream)
+	close(stderrstream)
+	close(resultstream)
+
+	return &run.Result{
+		OutputScanners: &run.StreamOutput{
+			StdOut: stdoutstream,
+			StdErr: stderrstream,
+			Result: resultstream,
+		},
+	}, nil
+}
+
+func setupTestRunner(t *testing.T, runner *testRunner) {
+	t.Helper()
+	oldClient := run.Client
+	run.Client = runner
+	t.Cleanup(func() {
+		run.Client = oldClient
+	})
+}
+
+func TestLaunchScriptRunner(t *testing.T) {
+	ctx := context.Background()
+	event := "startup"
+
+	tests := []struct {
+		name        string
+		runner      *testRunner
+		mdsClient   *MDSClient
+		wantCommand string
+		wantArgs    []string
+		wantErr     bool
+	}{
+		{
+			name:        "core_plugin_enabled",
+			runner:      &testRunner{},
+			mdsClient:   &MDSClient{instanceEnable: true},
+			wantCommand: metadataScriptRunnerNew,
+			wantArgs:    []string{event},
+			wantErr:     false,
+		},
+		{
+			name:        "core_plugin_disabled",
+			runner:      &testRunner{},
+			mdsClient:   &MDSClient{instanceEnable: false},
+			wantCommand: metadataScriptRunnerLegacy,
+			wantArgs:    []string{event},
+			wantErr:     false,
+		},
+		{
+			name:        "mds_error",
+			runner:      &testRunner{},
+			wantCommand: metadataScriptRunnerLegacy,
+			wantArgs:    []string{event},
+			mdsClient:   &MDSClient{throwErr: true},
+			wantErr:     false,
+		},
+		{
+			name:        "runner_error",
+			runner:      &testRunner{returnErr: true},
+			wantCommand: metadataScriptRunnerNew,
+			wantArgs:    []string{event},
+			mdsClient:   &MDSClient{instanceEnable: true},
+			wantErr:     true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			setupTestRunner(t, test.runner)
+			err := launchScriptRunner(ctx, test.mdsClient, event)
+			if (err == nil) == test.wantErr {
+				t.Errorf("launchScriptRunner(ctx, %+v, %q) error = %v, want %v", test.mdsClient, event, err, test.wantErr)
+			}
+			if test.runner.seenOutputType != run.OutputStream {
+				t.Errorf("launchScriptRunner(ctx, %+v, %q) executed output type = %v, want %v", test.mdsClient, event, test.runner.seenOutputType, run.OutputStream)
+			}
+			if test.runner.seenCommand != test.wantCommand {
+				t.Errorf("launchScriptRunner(ctx, %+v, %q) executed command = %q, want %q", test.mdsClient, event, test.runner.seenCommand, test.wantCommand)
+			}
+			if diff := cmp.Diff(test.runner.seenArgs, test.wantArgs); diff != "" {
+				t.Errorf("launchScriptRunner(ctx, %+v, %q) executed args = %v, want %v", test.mdsClient, event, test.runner.seenArgs, test.wantArgs)
+			}
+		})
+	}
+}
