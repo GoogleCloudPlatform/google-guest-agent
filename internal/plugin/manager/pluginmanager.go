b7a93541601bf949e85ba72567d6845b3ac90d5a
Andras Kerekes <akerekes@google.com>
2026-08-04 15:41:40 -0700

    Consolidate PluginManager initialization and make test monitor assertions hermetic.
    PiperOrigin-RevId: 959268872

--- internal/plugin/manager/pluginmanager.go
+++ internal/plugin/manager/pluginmanager.go
@@ -133,8 +133,8 @@ func Instance() *PluginManager {
 	return pluginManager
 }
 
-func init() {
-	pluginManager = &PluginManager{
+func newPluginManager() *PluginManager {
+	return &PluginManager{
 		protocol:                 tcpProtocol,
 		pluginMonitors:           make(map[string]string),
 		pluginMetricsMonitors:    make(map[string]string),
@@ -142,6 +142,10 @@ func init() {
 		inProgressPluginRequests: make(map[string]bool),
 		requestCount:             make(map[acpb.ConfigurePluginStates_Action]map[bool]int),
 	}
+}
+
+func init() {
+	pluginManager = newPluginManager()
 }
 
 func (m *PluginManager) setInstanceID(id string) {
@@ -163,16 +167,22 @@ func (m *PluginManager) currentInstanceID() string {
 // required. InitAdHocPluginManager skips some initialization steps like
 // scheduling plugin monitors that are not required for ad-hoc requests.
 func InitAdHocPluginManager(ctx context.Context, instanceID string) (*PluginManager, error) {
+	return pluginManager.initAdHoc(ctx, instanceID)
+}
+
+func (m *PluginManager) initAdHoc(ctx context.Context, instanceID string) (*PluginManager, error) { // NOGOUNUSED
 	galog.Infof("Initializing ad-hoc plugin manager for instance %q", instanceID)
 
-	pluginManager.setInstanceID(instanceID)
+	m.setInstanceID(instanceID)
 	plugins, err := load(agentPluginState())
 	if err != nil {
 		return nil, fmt.Errorf("unable to load existing plugin state: %w", err)
 	}
-	pluginManager.plugins = plugins
+	m.mu.Lock()
+	m.plugins = plugins
+	m.mu.Unlock()
 
-	return pluginManager, nil
+	return m, nil
 }
 
 // StopPlugin stops the plugin. This is used for ad-hoc requests that does not
@@ -199,15 +209,19 @@ func (m *PluginManager) StopPlugin(ctx context.Context, name string) error {
 // ACS is disabled. Plugin Manager will be initialized during early Guest Agent
 // startup to configure the core plugins.
 func InitPluginManager(ctx context.Context, instanceID string) (*PluginManager, error) {
+	return pluginManager.init(ctx, instanceID)
+}
+
+func (m *PluginManager) init(ctx context.Context, instanceID string) (*PluginManager, error) {
 	version := cfg.Retrieve().Core.Version
 	galog.Infof("Initializing plugin manager for instance %q, agent version: %q", instanceID, version)
-	pluginManager.setInstanceID(instanceID)
+	m.setInstanceID(instanceID)
 
 	// Cleanup old plugin state in a separate goroutine. This operation is not
 	// critical for plugin manager initialization and should not block it.
 	stateParentDir := filepath.Dir(baseState())
 	go func() {
-		if err := pluginManager.cleanupOldState(ctx, stateParentDir); err != nil {
+		if err := m.cleanupOldState(ctx, stateParentDir); err != nil {
 			galog.Errorf("Failed to cleanup old plugin state: %v", err)
 		}
 	}()
@@ -218,30 +232,32 @@ func InitPluginManager(ctx context.Context, instanceID string) (*PluginManager, 
 		return nil, fmt.Errorf("unable to load existing plugin state: %w", err)
 	}
 
-	pluginManager.plugins = plugins
+	m.mu.Lock()
+	m.plugins = plugins
+	m.mu.Unlock()
 
 	// Subscribe to cleanup event. This needs to happen after plugin manager is
 	// initialized, or we may accidentally cleanup plugins that shouldn't be removed.
-	scheduler.ScheduleJobs(ctx, []scheduler.Job{newCleanupJob(pluginManager)}, false)
+	scheduler.ScheduleJobs(ctx, []scheduler.Job{newCleanupJob(m)}, false)
 
 	if err := RegisterCmdHandler(ctx); err != nil {
 		return nil, fmt.Errorf("failed to register plugin command handler: %w", err)
 	}
 
 	wg := sync.WaitGroup{}
-	pluginManager.pendingPluginRevisionsMu.Lock()
+	m.pendingPluginRevisionsMu.Lock()
 
 	for _, p := range plugins {
-		pluginManager.inProgressPluginRequests[p.Name] = true
+		m.inProgressPluginRequests[p.Name] = true
 		wg.Add(1)
 
 		go func(p *Plugin) {
 			// Regardless of the outcome, we should remove the plugin from the pending
 			// list as request is no longer in process for this plugin.
 			defer func() {
-				pluginManager.pendingPluginRevisionsMu.Lock()
-				defer pluginManager.pendingPluginRevisionsMu.Unlock()
-				delete(pluginManager.inProgressPluginRequests, p.Name)
+				m.pendingPluginRevisionsMu.Lock()
+				defer m.pendingPluginRevisionsMu.Unlock()
+				delete(m.inProgressPluginRequests, p.Name)
 				wg.Done()
 			}()
 
@@ -259,31 +275,31 @@ func InitPluginManager(ctx context.Context, instanceID string) (*PluginManager, 
 			if err := connectOrReLaunch(ctx, p); err != nil {
 				galog.Errorf("Failed to connect or relaunch plugin %q: %v", p.FullName(), err)
 			} else {
-				pluginManager.startPluginSchedulers(ctx, p)
+				m.startPluginSchedulers(ctx, p)
 			}
 		}(p)
 	}
-	pluginManager.IsInitialized.Store(true)
-	pluginManager.pendingPluginRevisionsMu.Unlock()
+	m.IsInitialized.Store(true)
+	m.pendingPluginRevisionsMu.Unlock()
 	wg.Wait()
 
 	// TCP is always supported, so if the forced connection type is TCP, we can
 	// return early.
 	connType := cfg.Retrieve().Plugin.ConnectionType
 	if connType == tcpProtocol {
-		pluginManager.protocol = tcpProtocol
-		return pluginManager, nil
+		m.protocol = tcpProtocol
+		return m, nil
 	}
 
 	// If the connection type is not TCP, check if UDS is supported and set the
 	// protocol to UDS if it is. Otherwise, fallback to TCP.
 	if isUDSSupported() {
-		pluginManager.protocol = udsProtocol
+		m.protocol = udsProtocol
 	} else {
 		galog.Debugf("UDS is not supported, fallback to TCP")
-		pluginManager.protocol = tcpProtocol
+		m.protocol = tcpProtocol
 	}
-	return pluginManager, nil
+	return m, nil
 }
 
 // RemoveAllDynamicPlugins filters out core plugins and triggers plugin manager
